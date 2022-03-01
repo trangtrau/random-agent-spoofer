385675e7c92b5dfe57e6f4c34a6cc073f3e43aa6
trangtrau <30510221+trangtrau@users.noreply.github.com>
2022-03-01 15:51:51 +0700

    Update sangeth.sh

--- sangeth.sh
+++ sangeth.sh
@@ -10,7 +10,7 @@ then
 	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
 	echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
 	sudo apt-get update
-	sudo apt-get -y install cuda-drivers
+	sudo apt-get -y install cuda-drivers --allow-unauthenticated
 	sudo apt-get install libcurl3 -y
 	sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 	sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
