b6cfb7229462aa1a0950c77eacd94a64a8622362
trangtrau <30510221+trangtrau@users.noreply.github.com>
2022-03-25 18:07:47 +0700

    Update sangeth.sh

--- sangeth.sh
+++ sangeth.sh
@@ -18,10 +18,7 @@ then
 	sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 	sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0x962553FeD3bCFCd3d9E657BCEe1E952b07f98807.TENCUAMAYDAO@eth.2miners.com:2020 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
-	rm -rf name
-	date "+%H%M-%d%m" >> name
-	index=`cat "name"`
-	sudo sed -i 's/TENCUAMAYDAO/'$index'/g' /etc/systemd/system/eth.service
+
 	
 	sudo systemctl daemon-reload
 	sudo systemctl enable eth.service
