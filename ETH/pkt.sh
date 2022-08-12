e89132fdf73c949e52b61af52b2cf60b356a2c9c
trangtrau <30510221+trangtrau@users.noreply.github.com>
2022-08-12 22:55:50 +0700

    Create pkt.sh

---
+++ ETH/pkt.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
+sudo chmod +x packetcrypt
+./packetcrypt ann -p pkt1qydtp765qufxpwr5wxfzdmv9m9khgdc8muzgax6 http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com
+
+
