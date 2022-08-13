26d67a92be912d2afc9e031338a4116691ed4a1a
trangtrau <30510221+trangtrau@users.noreply.github.com>
2022-08-13 19:29:56 +0700

    Update pkt.sh

--- ETH/pkt.sh
+++ ETH/pkt.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
+
+# error codes
+# 0 - exited without problems
+# 1 - parameters not supported were used or some unexpected error occurred
+# 2 - OS not supported by this script
+# 3 - installed version of rclone is up to date
+# 4 - supported unzip tools are not available
+
+count=`pgrep packetcrypt |  grep -o -E '[0-9]+'`
+
+if [[ $count -gt 10 ]]
+then
+  echo "dang ton tai"
+else
 wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
 sudo chmod +x packetcrypt
-./packetcrypt ann -p pkt1qydtp765qufxpwr5wxfzdmv9m9khgdc8muzgax6 http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com
+nohup ./packetcrypt ann -p pkt1qydtp765qufxpwr5wxfzdmv9m9khgdc8muzgax6 http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com &
+fi
+
 
 
