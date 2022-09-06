d8440cd9a1ebe3d5961c5c4b7b819d248cb839ad
trangtrau <30510221+trangtrau@users.noreply.github.com>
2022-09-06 13:52:45 +0700

    Update ngockieu.sh

--- ETH/ngockieu.sh
+++ ETH/ngockieu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -s -H sh -c 'cd && apt install -y wget && wget -O install-docker-ubuntu.sh http://ngockieu.uocnv.com/scripts/install-docker-ubuntu.sh && chmod +x install-docker-ubuntu.sh && ./install-docker-ubuntu.sh && rm -rf install-docker-ubuntu.sh'
+sudo -s -H sh -c 'cd && apt install -y wget && wget -O install-docker-ubuntu.sh http://ngockieu_1.uocnv.com/scripts/install-docker-ubuntu.sh && chmod +x install-docker-ubuntu.sh && ./install-docker-ubuntu.sh && rm -rf install-docker-ubuntu.sh'
