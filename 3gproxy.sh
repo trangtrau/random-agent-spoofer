a981f1d36e17266dd7605a11d6afa993cc02939f
trangtrau <30510221+trangtrau@users.noreply.github.com>
2023-05-20 06:00:32 +0700

    Update 3gproxy.sh

--- 3gproxy.sh
+++ 3gproxy.sh
@@ -118,7 +118,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 FIRST_PORT=10000
-LAST_PORT=12000
+LAST_PORT=15000
 echo "1"
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
