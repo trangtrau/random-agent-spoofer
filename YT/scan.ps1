26e788d4b8daab27ee060f1bf36354eb6fbb203a
trangtrau <30510221+trangtrau@users.noreply.github.com>
2024-12-19 16:02:24 +0700

    Update scan.ps1

--- YT/scan.ps1
+++ YT/scan.ps1
@@ -2,13 +2,16 @@
 $downloadPath = "$env:TEMP\Downloads"
 New-Item -ItemType Directory -Path $downloadPath -Force
 
-Write-Host "Tải xuống file cài đặt Node.js và Python..." -ForegroundColor Green
+Write-Host "Tải xuống file cài đặt Node.js, Python và Brave Browser..." -ForegroundColor Green
 
 # Tải file Node.js
 Invoke-WebRequest -Uri "https://nodejs.org/dist/v20.18.1/node-v20.18.1-x64.msi" -OutFile "$downloadPath\node-v20.18.1-x64.msi"
 
 # Tải file Python
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.10.6/python-3.10.6-amd64.exe" -OutFile "$downloadPath\python-3.10.6-amd64.exe"
+
+# Tải file Brave Browser
+Invoke-WebRequest -Uri "https://referrals.brave.com/latest/BraveBrowserSetup-BRV010.exe" -OutFile "$downloadPath\BraveBrowserSetup-BRV010.exe"
 
 Write-Host "Tải xuống hoàn tất." -ForegroundColor Green
 
@@ -20,7 +23,15 @@ Write-Host "Cài đặt Python..." -ForegroundColor Green
 # Cài đặt Python
 Start-Process -FilePath "$downloadPath\python-3.10.6-amd64.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
 
-Write-Host "Cài đặt Node.js và Python hoàn tất." -ForegroundColor Green
+Write-Host "Cài đặt Brave Browser..." -ForegroundColor Green
+# Cài đặt Brave Browser
+Start-Process -FilePath "$downloadPath\BraveBrowserSetup-BRV010.exe" -ArgumentList "/silent" -Wait
+
+Write-Host "Cài đặt Node.js, Python và Brave Browser hoàn tất." -ForegroundColor Green
+
+# Cập nhật lại biến môi trường PATH trong phiên PowerShell hiện tại
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine) + ";" + [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+Write-Host "Cập nhật PATH hoàn tất. Giờ đây bạn có thể sử dụng npm và pip mà không cần khởi động lại console." -ForegroundColor Green
 
 Write-Host "Cài đặt các gói NPM..." -ForegroundColor Green
 # Cài đặt các gói NPM
