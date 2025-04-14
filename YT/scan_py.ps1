446e6865cd0f3879884a9d6b888aee62d65728d1
trangtrau <30510221+trangtrau@users.noreply.github.com>
2025-04-15 06:59:34 +0700

    Update scan_py.ps1

--- YT/scan_py.ps1
+++ YT/scan_py.ps1
@@ -10,7 +10,7 @@ Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.10.6/python-3.10.6-a
 # Tải file Brave Browser
 #Invoke-WebRequest -Uri "https://referrals.brave.com/latest/BraveBrowserSetup-BRV010.exe" -OutFile "$downloadPath\BraveBrowserSetup-BRV010.exe"
 Invoke-WebRequest -Uri "https://github.com/trangtrau/random-agent-spoofer/raw/refs/heads/master/YT/chrome_setup.exe" -OutFile "$downloadPath\chrome_setup.exe"
-Invoke-WebRequest -Uri "https://www.dropbox.com/scl/fi/4457f7aymgul350cxkuuo/nimo_tool.zip?rlkey=k0kq9xacj3132qchb1p9armft&st=up6tpxiu&dl=1" -OutFile "$downloadPath\nimo_tool.zip"
+#Invoke-WebRequest -Uri "https://www.dropbox.com/scl/fi/4457f7aymgul350cxkuuo/nimo_tool.zip?rlkey=k0kq9xacj3132qchb1p9armft&st=up6tpxiu&dl=1" -OutFile "$downloadPath\nimo_tool.zip"
 Invoke-WebRequest -Uri "https://www.dropbox.com/scl/fi/19n0o7j4p8ssstopg1wba/gpm.zip?rlkey=l365fzib20c9aa4de22ak091x&st=ayrmum3v&dl=1" -OutFile "$downloadPath\gpm.zip"
 
 Write-Host "Tải xuống hoàn tất." -ForegroundColor Green
@@ -28,7 +28,7 @@ Start-Process -FilePath "$downloadPath\python-3.10.6-amd64.exe" -ArgumentList "/
 $destination = Join-Path -Path ([Environment]::GetFolderPath("Desktop")) -ChildPath "Nimo"
 $destination2 = Join-Path -Path ([Environment]::GetFolderPath("Desktop")) -ChildPath "GPM"
 
-Expand-Archive -Path "$downloadPath\nimo_tool.zip" -DestinationPath $destination -Force
+#Expand-Archive -Path "$downloadPath\nimo_tool.zip" -DestinationPath $destination -Force
 Expand-Archive -Path "$downloadPath\gpm.zip" -DestinationPath $destination2 -Force
 
 # Cập nhật lại biến môi trường PATH trong phiên PowerShell hiện tại
